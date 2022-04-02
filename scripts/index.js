a4eacaba92abff646276464004462a59f534cd6b
vats1812 <68377043+vats1812@users.noreply.github.com>
2022-04-02 22:59:26 +0530

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,48 @@
+//main countdown
+
+document.addEventListener('DOMContentLoaded', () => {
+    var timeOfHackathon = new Date(2022, 3, 23, 10, 0) / 1000;
+    // var flipdown = new FlipDown(timeOfHackathon, 'countdown-main').start();
+    new FlipDown(timeOfHackathon, 'countdown-main', {
+        theme: 'light'
+    }).start();
+});
+
+
+// TIMELINE JAVASCRIPT
+
+let timeline = document.getElementById("timeline");
+let timeline2 = document.getElementById("timeline2");
+
+
+let day1 = document.getElementById("day1");
+let day2 = document.getElementById("day2");
+
+
+
+day1.addEventListener("click", function() {
+    if (timeline.style.display === "none") {
+        timeline.style.display = "block";
+        timeline2.style.display = "none";
+        day1.classList.add("active");
+        day2.classList.remove("active");
+    } else {
+        timeline.style.display = "block";
+        timeline2.style.display = "none";
+        day1.classList.add("active");
+        day2.classList.remove("active");
+    }
+});
+day2.addEventListener("click", function() {
+    if (timeline2.style.display === "none") {
+        timeline2.style.display = "block";
+        timeline.style.display = "none";
+        day2.classList.add("active");
+        day1.classList.remove("active");
+    } else {
+        timeline2.style.display = "block";
+        timeline.style.display = "none";
+        day2.classList.add("active");
+        day1.classList.remove("active");
+    }
+});
